1af46f378dec6abe098b644f16b809d30185e537
Stuart Stobie <Stuart.Stobie@Gamesys.co.uk>
2025-10-07 20:01:27 +0100

    create challenge

--- backend/src/services/gameInfo.ts
+++ backend/src/services/gameInfo.ts
@@ -4,28 +4,10 @@ import { GameInfo } from "../types/Game";
 import { isValidCategory } from "../utils";
 
 export const getGameInfo = (req: Request, res: Response) => {
-    const { category, gameId } = req.params;
-  
-    if (!isValidCategory(category!)) {
-      return res.status(404).json({ error: 'Invalid category' });
-    }
+    return res.status(404).json({ error: 'Game not found' });
     
-    const games = gameInfoData[category];
-    const game = games?.find(g => g.id === gameId);
+    // TODO: Implement this
+    // const gameInfo: GameInfo = {} as GameInfo;
     
-    if (!game) {
-      return res.status(404).json({ error: 'Game not found' });
-    }
-    
-    const gameInfo: GameInfo = {
-      details: {
-        description: game.description,
-        rtp: game.rtp,
-        minBet: game.minBet,
-        maxBet: game.maxBet,
-        features: game.features || []
-      }
-    };
-    
-    return res.json(gameInfo);
+    // return res.json(gameInfo);
 }
