3ee945ada9508251b22417c71234e2889eaa894b
Stuart Stobie <Stuart.Stobie@Gamesys.co.uk>
2025-10-08 10:22:42 +0100

    even more challenge

--- backend/src/services/gameInfo.ts
+++ backend/src/services/gameInfo.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
-import gameInfoData from "../data/gameInfo";
-import { GameInfo } from "../types/Game";
-import { isValidCategory } from "../utils";
 
 export const getGameInfo = (req: Request, res: Response) => {
     return res.status(404).json({ error: 'Game not found' });
-    
+
     // TODO: Implement this
-    // const gameInfo: GameInfo = {} as GameInfo;
+    // const gameInfo: GameInfo
     
     // return res.json(gameInfo);
 }
